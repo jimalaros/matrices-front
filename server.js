c2dc04f98179dfae07d5c75323a4b8223bf97397
jimalaros <jimalaros25@gmail.com>
2022-11-24 11:09:46 -0500

    first commit

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.static(__dirname + '/dist/form'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/form'));
+  res.sendFile(path.join(__dirname + '/dist/form/index.html'));
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
